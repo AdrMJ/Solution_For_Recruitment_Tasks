0ed4c5673b8dd3578ee7352f34b77a1d927f1113
AdrMJ <101901718+AdrMJ@users.noreply.github.com>
2022-10-10 14:46:37 +0200

    Repair code
    Fast repair code

--- Zadania rekrutacyjne/kanyeapi/src/main/java/Main.java
+++ Zadania rekrutacyjne/kanyeapi/src/main/java/Main.java
@@ -8,11 +8,9 @@ import java.util.Scanner;
 
 import com.google.gson.Gson;
 
-
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        V_Quotes v_quotes = new V_Quotes();
         Transcript transcript = new Transcript();
         transcript.setUrl("https://github.com/ajzbc/kanye.rest/blob/master/quotes.json");
 
@@ -31,22 +29,14 @@ public class Main {
 
             HttpClient httpClient = HttpClient.newHttpClient();
             HttpResponse<String> postResponse = httpClient.send(postRequest, BodyHandlers.ofString());
-            for(int i = 0; i<v_quotes.size();i++){
-                if (v_quotes.get(i)==transcript.getQuote()) {
-                    next = "NEXT";
-                    if(i==v_quotes.size()){
-                    continue;
-                    }
-                } else {
 
-                    transcript = gson.fromJson(postResponse.body(), Transcript.class);
-                    v_quotes.add(transcript.getQuote());
-                    System.out.println(transcript.getQuote());
-                    System.out.println("Napisz \"next\", aby zobaczyć kolejny cytat");
+            transcript = gson.fromJson(postResponse.body(), Transcript.class);
 
-                    next = scan.nextLine();
-                }
-            }
+            System.out.println(transcript.getQuote());
+            System.out.println("Napisz \"next\", aby zobaczyć kolejny cytat");
+
+            next = scan.nextLine();
+
         } while (next.toUpperCase().equals("NEXT"));
 
         scan.close();
